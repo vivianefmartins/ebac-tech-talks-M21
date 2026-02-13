c5ba5179ae2645e22b1f8afcc7ce8b0002286271
Viviane <viane.fmar@gmail.com>
2026-02-12 23:31:27 -0300

    criando contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('olá, mundo!');
+AOS.init();
+
+const dataDoEvento = new Date("Feb 22, 2026 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteEvento % horaEmMs) / minEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteEvento % minEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
